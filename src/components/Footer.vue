d13cb2f47dd99686b3012158443ac43429a66305
JoshuaTakahashi <ruffy.joshua@gmail.com>
2020-03-03 10:08:29 +0800

    GroceryProject with Router
    Grocery Project with router

---
+++ src/components/Footer.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="headerStyle">
+    <footer>
+      <p v-html="copyRtSymbol"></p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      copyRtSymbol: "&copy;BT3103"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+footer {
+  background: rgb(56, 122, 72);
+  padding: 10px;
+  border-style: solid;
+  border-color: black;
+  border-width: 2px;
+}
+p {
+  align-content: center;
+  color: ivory;
+}
+</style>
